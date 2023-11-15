8bb0ed6e24b1f897b3ef237daa5e2c32ab110595
Noel Lopes <noel@ipg.pt>
2023-11-15 14:46:09 +0000

    Created a model to represent the class group

---
+++ Models/Group.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Supermarket.Models {
+    public class Group {
+        public int GroupId {  get; set; }
+
+        public int Number { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        public required string Objectives { get; set; }
+    }
+}
