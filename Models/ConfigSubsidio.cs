59aa718bd9c323f2ac91a8634a6eee389b734411
adriano <menicorreiaco@gmail.com>
2023-11-22 16:15:38 +0000

    Modelo de Configuraçao de Subsidio

---
+++ Models/ConfigSubsidio.cs
@@ -0,0 +1,10 @@
+﻿namespace Supermarket.Models
+{
+    public class ConfigSubsidio
+    {
+
+        public int ConfigSUbId { get; set; }
+        public DateTime HorasMinTrab { get; set; }
+        public float valorSubsidio { get; set; }
+    }
+}
