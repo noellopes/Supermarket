7aac6e0a7a1fda8d6fc6922f4d42a86002424682
LucasSatlher <lucsatlher@gmail.com>
2023-11-22 16:02:23 +0000

    Criado modelo "AvaliacaoFuncionario"

---
+++ Models/AvaliacaoFuncionario.cs
@@ -0,0 +1,12 @@
+﻿namespace Supermarket.Models
+{
+    public class AvaliacaoFuncionario
+    {
+        public int AvaliacaoFuncId { get; set; }
+        
+        public string? Description { get; set; }
+        public int GradeNumber { get; set; }
+
+        public int FuncionarioId { get; set;}
+    }
+}
