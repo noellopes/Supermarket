0dbac043e6b8202507640574b75f7deb2bd45187
andregod <andre_godinho1@hotmail.com>
2023-11-22 16:11:35 +0000

    Created Schedule Model

---
+++ Models/HorarioAtendimento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Supermarket.Models;
+
+public class HorarioAtendimento
+{
+	public int HorarioId { get; set; }
+	[Required]
+	public DateTime DataInicio { get; set; }
+    [Required]
+    public DateTime DataFim { get; set; }
+	public int DeptID { get; set; }
+
+
+}
