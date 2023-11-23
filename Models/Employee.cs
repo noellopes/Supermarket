48a5225e02fdfa1384ae9729390d1783f3652146
Xiquinho32 <franciscofcp32@gmail.com>
2023-11-23 19:45:38 +0000

    Criação (Em comentário) das ICollections das outras tabelas.

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -57,22 +57,38 @@ namespace Supermarket.Models
         [Required]
         public Meal_Card Meal_Card { get; set; }
 
+
         //Ligação de 1 para 1 com Horario de atendimento (Schedule)
+        //[Required]
         //public Schedule Schedule { get; set; }
 
-
-
         //Buscar a coleção das folgas (1 para muitos)
-        //public ICollection<Day_Off>? Days_Off { get; set;}
+        //public ICollection<Day_Off> Days_Off { get; set;}
 
         //Buscar a coleção das Escalas (1 para muitos)
         //public ICollection<Scales>? Scales { get; set;}
 
+        //Buscar a coleção dos Pontos (1 para muitos)
+        //public ICollection<Point> Point { get; set;}
+
+        //Buscar a coleção dos Alerta (1 para muitos)
+        //public ICollection<Alerta> Alerta { get; set;}
+
+        //Buscar a coleção dos Abate_Funcionario (1 para muitos)
+        //public ICollection<Employee_Reduction> Employee_Reduction { get; set;}
 
 
-        //Reservas 
 
-        //Departamento_Funcionarios
+
+        //Buscar a coleção das Qualificação (Muitos para muitos)
+        //public ICollection<Qualifications> Qualifications { get; set;}
+
+        //Buscar a coleção do Departamento (Muitos para muitos)
+        //public ICollection<Departments> Departments { get; set;}
+
+
+
+
 
 
 
