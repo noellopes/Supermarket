2217f4df26ea67e6c7b2a0273872eab61f5b5bfb
João Gonçalves <josantiago2033@gmail.com>
2023-11-24 17:34:52 +0000

    Add ReduceProduct Model

---
+++ Models/ReduceProduct.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Supermarket.Models
+{
+    public class ReduceProduct
+    {
+        public int ReduceProductId { get; set; }
+
+        [Required]
+        public required string Reason { get; set; }
+
+        [Required]
+        public required string Status { get; set; } = "Pending";
+
+        public DateTime Date { get; set;} = DateTime.Now;
+
+        public int Quantity { get; set;}
+
+        public int EmployeeId { get; set; }
+
+        public Employee? Employee { get; set; }
+
+        public int ProductId { get; set; }
+
+        public Product? Product { get; set; }
+
+        public int? WarehouseSectionId { get; set; }
+
+        public WarehouseSection? WarehouseSection { get; set; }
+
+        public int? ShelfId { get; set; }
+
+        public Shelf? Shelf { get; set; }
+
+
+    }
+}
