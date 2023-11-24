3e9a5037e88bfba87de3d22bbf9f0192440c4936
Alexandre Rocha <1704920@sal.ipg.pt>
2023-11-24 09:33:55 +0000

    Created Ticket Models

---
+++ Models/Tickets.cs
@@ -0,0 +1,22 @@
+﻿namespace Supermarket.Models
+{
+    public class Tickets
+    {
+        public int TicketId { get; set; }
+
+        public DateTime PrintDate { get; set; }
+
+
+        public int TicketNumber { get; set;}
+
+        public string TicketState { get; set; }
+
+        public int DepartmentId { get; set; } 
+
+        public Departments Departments { get; set; }
+
+        public int ClientCard { get; set; }
+
+        //public ClientCard ClientCard { get; set; }
+    }
+}
