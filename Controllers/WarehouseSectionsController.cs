59c92dd33cc81cffe84373c4884fc7e1ec147688
PINELA <pinelaivo10@gmail.com>
2023-11-27 10:18:34 +0000

    Fix an error in the WarehouseSection controller in editing

--- Controllers/WarehouseSectionsController.cs
+++ Controllers/WarehouseSectionsController.cs
@@ -115,7 +115,7 @@ namespace Supermarket.Controllers
                 try
                 {
                     bool WarehouseSectionExists = await _context.WarehouseSection.AnyAsync(
-                    b => b.Description == warehouseSection.Description && b.WarehouseId == warehouseSection.WarehouseId);
+                    b => b.Description == warehouseSection.Description && b.WarehouseId == warehouseSection.WarehouseId && b.WarehouseSectionId != warehouseSection.WarehouseSectionId);
 
                     if (WarehouseSectionExists)
                     {
