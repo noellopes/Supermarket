7add9bcca11b04a9c27cbb97ad78b265cd67f02b
Eduardo <eduardohsp@live.com>
2023-11-28 20:25:46 +0000

    ADD Issue category model

---
+++ Models/IssueType.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Supermarket.Models
+{
+    public class IssueType
+    {
+        public int IssueTypeId { get; set; } // Issue Type Category Identifier
+
+        [Required]
+        [StringLength(50, MinimumLength = 7)]
+        public required string Name { get; set; } // Issue Type Category Name
+
+        [Required]
+        [MinLength(10)]
+        [Display(Name = "Issue Category Description")]
+        public string IssueDescription { get; set; } = string.Empty; // Issue Type Category Description   
+
+        public ICollection<IssueType>? IssueTypes { get; set; }
+    }
+}
