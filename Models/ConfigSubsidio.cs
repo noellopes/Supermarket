8e5f9b29bd2623c832d65b07c03cabb29bf4854d
adriano <menicorreiaco@gmail.com>
2023-11-28 18:24:24 +0000

    Craiafo CRUD para Configuaraçao de Subsidio

--- Models/ConfigSubsidio.cs
+++ Models/ConfigSubsidio.cs
@@ -3,7 +3,7 @@
     public class ConfigSubsidio
     {
 
-        public int ConfigSUbId { get; set; }
+        public int ConfigId { get; set; }
         public DateTime HorasMinTrab { get; set; }
         public float valorSubsidio { get; set; }
     }
