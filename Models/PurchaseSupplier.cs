37dd0062e1173756b63bea369d0323ba69d9dfda
Pedro Costa <1705064@sal.ipg.pt>
2023-11-28 23:24:54 +0000

    Criação dos modelos dos Suppliers e PurchaseSupplier

---
+++ Models/PurchaseSupplier.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Supermarket.Models
+{
+    public class PurchaseSupplier
+    {
+        public int PurchaseSupplierId { get; set; }
+        [Required]
+
+        public int SubTotal { get; set; }
+
+        [Required]
+        public int Total { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+        public int SupplierId { get; set; }
+
+        public Supplier Supplier { get; set; }
+
+    }
+
+}
---
+++ Models/Supplier.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Supermarket.Models
+{
+    public class Supplier
+    {
+
+        public int SupplierId { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public int Name { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 10)]
+        public string HeadQuarters { get; set; }
+
+        [Required]
+        [Range(0, 999999999)]
+        public int Telefone { get; set; }
+        public string Email { get; set; }
+
+
+
+    }
+}
