da99b599f9b79d5ea9c93efc1a2cdd7695eb3d89
LucasSatlher <lucsatlher@gmail.com>
2023-11-29 12:06:28 +0000

    Added EmployeeEvaluation CRUD
    Added controller and views to Employee Evaluation. Also, added a field "Employee id" to the model.

---
+++ Data/Migrations/Supermarket/20231129115603_EmployeeEvaluationChanged.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Supermarket.Data.Migrations.Supermarket
+{
+    /// <inheritdoc />
+    public partial class EmployeeEvaluationChanged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EmployeeId",
+                table: "avaliacaoFuncionarios",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmployeeId",
+                table: "avaliacaoFuncionarios");
+        }
+    }
+}
--- Models/EmployeeEvaluation.cs
+++ Models/EmployeeEvaluation.cs
@@ -5,8 +5,10 @@
         public int EmployeeEvaluationId { get; set; }
         
         public string? Description { get; set; }
+        [System.ComponentModel.DisplayName("Grade Number")]
         public int GradeNumber { get; set; }
 
-        //public int FuncionarioId { get; set;}
+        [System.ComponentModel.DisplayName("Employee Id")]
+        public int? EmployeeId { get; set;}
     }
 }
