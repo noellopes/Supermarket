fbfa1346a99efe62a2cec40bd99c6c28fda7c8c6
adriano <menicorreiaco@gmail.com>
2023-11-29 12:48:58 +0000

    correcao do Ultimo Commit no Model

--- Models/ConfigSubsidio.cs
+++ Models/ConfigSubsidio.cs
@@ -2,7 +2,6 @@
 {
     public class ConfigSubsidio
     {
-
         public int ConfigId { get; set; }
         public DateTime HorasMinTrab { get; set; }
         public float valorSubsidio { get; set; }
