706550587f528f14c1bb0dbbbb44f7ef77fb91a8
João Carlos Martins <jocafmartins@hotmail.com>
2023-12-05 22:39:43 +0000

    Create the clientcard model

---
+++ Models/ClientCard.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace Supermarket.Models
+{
+    public class ClientCard
+    {
+        public int ClientCard_Id { get; set; }
+
+        
+        [StringLength(6)]
+        required public string ClientCard_Number { get; set; }
+
+        public float  Balance { get; set; }
+    }
+}
