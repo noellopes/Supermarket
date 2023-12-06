67e6fc096b2bdd7cef10d4c79d56f24cc5e3ce8a
arman <armandojpazevedo@gmail.com>
2023-12-06 20:45:24 +0000

    Correction of model names, derived from underlines

---
+++ Models/CardMovement.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using System.Threading.Tasks;
+
+namespace Supermarket.Models
+{
+
+    public class CardMovement
+    {
+        public int CardMovementId { get; set; }
+
+        public DateTime Movement_Date { get; set; }
+
+        public int Value { get; set; }
+
+        public string Description { get; set; }
+
+        public string Type {  get; set; } 
+
+        public int Card_Id { get; set; }
+
+        public MealCard MealCard { get; set; }
+
+    }
+}
