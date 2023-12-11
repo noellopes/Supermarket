92dce90587ab69bc8c404a6165af2f909720f2ab
arman <armandojpazevedo@gmail.com>
2023-12-11 22:11:33 +0000

    Keep Fixing

---
+++ Data/SupermarketDbContext.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Supermarket.Models;
+
+
+namespace Supermarket.Data
+{
+    public class SupermarketDbContext : DbContext
+    {
+        public SupermarketDbContext (DbContextOptions<SupermarketDbContext> options)
+            : base(options)
+        {
+        }
+
+       public DbSet<Supermarket.Models.Folga> Folga { get; set; } = default!;
+
+        public DbSet<Supermarket.Models.Employee> Employee { get; set; } = default!;
+
+        public DbSet<Supermarket.Models.ConfigSubsidio> ConfigSubsidio { get; set; } = default!;
+
+        public DbSet<Supermarket.Models.SubsidySetup> SubsidySetup { get; set; } = default!;
+
+
+
+
+
+
+    }
+}
