429acb2453d04670415f66fb9e2b39356c463a52
Sofia Belon <113783283+SofiaBelon@users.noreply.github.com>
2023-12-12 13:01:58 +0000

    Criacao classe ponto

---
+++ Controllers/PontoesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Supermarket.Data;
+using Supermarket.Models;
+
+namespace Supermarket.Controllers
+{
+    public class PontoesController : Controller
+    {
+        private readonly SupermarketDbContext _context;
+
+        public PontoesController(SupermarketDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pontoes
+        public async Task<IActionResult> Index()
+        {
+              return _context.Ponto != null ? 
+                          View(await _context.Ponto.ToListAsync()) :
+                          Problem("Entity set 'SupermarketDbContext.Ponto'  is null.");
+        }
+
+        // GET: Pontoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Ponto == null)
+            {
+                return NotFound();
+            }
+
+            var ponto = await _context.Ponto
+                .FirstOrDefaultAsync(m => m.PontoId == id);
+            if (ponto == null)
+            {
+                return NotFound();
+            }
+
+            return View(ponto);
+        }
+
+        // GET: Pontoes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pontoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PontoId,EmployeeId,Date,CheckInTime,CheckOutTime,LunchStartTime,LunchEndTime,DayBalance,Status,Justificative,CheckInCoordenates,CheckOutCoordenates")] Ponto ponto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ponto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ponto);
+        }
+
+        // GET: Pontoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Ponto == null)
+            {
+                return NotFound();
+            }
+
+            var ponto = await _context.Ponto.FindAsync(id);
+            if (ponto == null)
+            {
+                return NotFound();
+            }
+            return View(ponto);
+        }
+
+        // POST: Pontoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PontoId,EmployeeId,Date,CheckInTime,CheckOutTime,LunchStartTime,LunchEndTime,DayBalance,Status,Justificative,CheckInCoordenates,CheckOutCoordenates")] Ponto ponto)
+        {
+            if (id != ponto.PontoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ponto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PontoExists(ponto.PontoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ponto);
+        }
+
+        // GET: Pontoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Ponto == null)
+            {
+                return NotFound();
+            }
+
+            var ponto = await _context.Ponto
+                .FirstOrDefaultAsync(m => m.PontoId == id);
+            if (ponto == null)
+            {
+                return NotFound();
+            }
+
+            return View(ponto);
+        }
+
+        // POST: Pontoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Ponto == null)
+            {
+                return Problem("Entity set 'SupermarketDbContext.Ponto'  is null.");
+            }
+            var ponto = await _context.Ponto.FindAsync(id);
+            if (ponto != null)
+            {
+                _context.Ponto.Remove(ponto);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PontoExists(int id)
+        {
+          return (_context.Ponto?.Any(e => e.PontoId == id)).GetValueOrDefault();
+        }
+    }
+}
