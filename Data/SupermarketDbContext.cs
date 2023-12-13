8da3308a8c21a2a07cd2fb051105814da4c55059
arman <armandojpazevedo@gmail.com>
2023-12-13 16:52:36 +0000

    Fix Merge

---
+++ Data/SupermarketDbContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Supermarket.Models;
+
+namespace Supermarket.Data
+{
+    public class SupermarketDbContext : DbContext
+    {
+        public SupermarketDbContext (DbContextOptions<SupermarketDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<EmployeeEvaluation>().HasKey(EE => EE.EmployeeEvaluationId);
+            modelBuilder.Entity<MealCard>().HasKey(MC => MC.MealCardId);
+        }
+
+        public DbSet<Folga> Folga { get; set; } = default!;
+
+
+        public DbSet<Supermarket.Models.IssueType> IssueType { get; set; } = default!;
+
+        public DbSet<Supermarket.Models.Issues> Issues { get; set; } = default!;
+
+        public DbSet<Supermarket.Models.ProductExpiration> ProductExpiration { get; set; } = default!;
+
+        public DbSet<Supermarket.Models.Funcao> Funcao { get; set; } = default!;
+
+        public DbSet<EmployeeEvaluation> AvaliacaoFuncionarios { get; set; } = default!;
+
+        public DbSet<Employee> Funcionarios { get; set; } = default!;
+
+        public DbSet<ProductDiscount> ProductDiscount { get; internal set; }
+
+        public DbSet<Supermarket.Models.Product> Product { get; set; } = default!;
+
+        public DbSet<Supermarket.Models.Hallway> Hallway { get; set; } = default!;
+
+        public DbSet<Supermarket.Models.Shelf> Shelf { get; set; } = default!;
+
+        public DbSet<Supermarket.Models.Shelft_ProductExhibition> Shelft_ProductExhibition { get; set; } = default!;
+
+        public DbSet<Supermarket.Models.Brand> Brand { get; set; } = default!;
+
+        public DbSet<Supermarket.Models.Category> Category { get; set; } = default!;
+
+        public DbSet<Supermarket.Models.ReduceProduct> ReduceProduct { get; set; } = default!;
+
+        public DbSet<Supermarket.Models.Warehouse> Warehouse { get; set; } = default!;
+
+        public DbSet<Supermarket.Models.WarehouseSection> WarehouseSection { get; set; } = default!;
+
+        public DbSet<Supermarket.Models.WarehouseSection_Product> WarehouseSection_Product { get; set; } = default!;
+
+        public DbSet<Supermarket.Models.Store> Store { get; set; } = default!;
+
+        public DbSet<ClientCard> ClientCard { get; set; }
+
+        public DbSet<CategoryDiscount> CategoryDiscounts { get; set; }
+    }
+}
