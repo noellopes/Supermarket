13b3d5ed7e584468ac02d509fa1138aa45531722
AntonioCerdeira <antonio.cerdeira@gmail.com>
2023-12-13 16:09:27 +0000

    Update Crud Departments

--- Data/GroupsDbContext.cs
+++ Data/GroupsDbContext.cs
@@ -16,5 +16,7 @@ namespace Supermarket.Data
 
         public DbSet<Supermarket.Models.Group> Group { get; set; } = default!;
 
+        public DbSet<Supermarket.Models.Departments> Departments { get; set; } = default!;
+
     }
 }
