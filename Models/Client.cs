e0cdeec934cd1887b5c59e6f6c68c9badbe5fefc
João Carlos Martins <jocafmartins@hotmail.com>
2023-12-14 17:56:08 +0000

    Create Crud Client and Correction of CURD ClientCard

---
+++ Models/Client.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Supermarket.Models
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+
+        [Required]
+        [StringLength(50),MinLength(3)]
+        required public string ClientName { get; set; }
+        [Required]
+        [StringLength(50), MinLength(3)]
+        required public string ClientAdress { get; set; }
+        [Required]
+        [StringLength(50), MinLength(3)]
+        required public string ClientEmail { get; set; }
+        [Required]
+        required public DateTime ClientBirth {  get; set; } 
+
+
+    }
+}
--- Models/ClientCard.cs
+++ Models/ClientCard.cs
@@ -5,13 +5,16 @@ namespace Supermarket.Models
 {
     public class ClientCard
     {
-        [Key]
-        public int ClientCardId { get; set; }
 
+        public int ClientCardId { get; set; }
+        public  Client? Client {get;  set;}
         
         [StringLength(6)]
         required public string ClientCardNumber { get; set; }
 
-        public float  Balance { get; set; }
+        public float Balance { get; set; } = 0;
+
+        public Boolean Estado { get; set; } = true;
+        
     }
 }
