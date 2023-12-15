dada167a3b7bdc791d8882203676c8de8b771f50
andregod <andre_godinho1@hotmail.com>
2023-12-15 11:42:25 +0000

    Add schedules

---
+++ Controllers/SchedulesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Supermarket.Data;
+using Supermarket.Models;
+
+namespace Supermarket.Controllers
+{
+    public class SchedulesController : Controller
+    {
+        private readonly SupermarketDbContext _context;
+
+        public SchedulesController(SupermarketDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Schedules
+        public async Task<IActionResult> Index()
+        {
+              return _context.Schedule != null ? 
+                          View(await _context.Schedule.ToListAsync()) :
+                          Problem("Entity set 'SupermarketDbContext.Schedule'  is null.");
+        }
+
+        // GET: Schedules/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Schedule == null)
+            {
+                return NotFound();
+            }
+
+            var schedule = await _context.Schedule
+                .FirstOrDefaultAsync(m => m.ScheduleId == id);
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+
+            return View(schedule);
+        }
+
+        // GET: Schedules/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Schedules/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ScheduleId,StartDate,EndDate,DailyStartTime,DailyFinishTime,IDDepartments")] Schedule schedule)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(schedule);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(schedule);
+        }
+
+        // GET: Schedules/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Schedule == null)
+            {
+                return NotFound();
+            }
+
+            var schedule = await _context.Schedule.FindAsync(id);
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+            return View(schedule);
+        }
+
+        // POST: Schedules/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ScheduleId,StartDate,EndDate,DailyStartTime,DailyFinishTime,IDDepartments")] Schedule schedule)
+        {
+            if (id != schedule.ScheduleId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(schedule);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ScheduleExists(schedule.ScheduleId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(schedule);
+        }
+
+        // GET: Schedules/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Schedule == null)
+            {
+                return NotFound();
+            }
+
+            var schedule = await _context.Schedule
+                .FirstOrDefaultAsync(m => m.ScheduleId == id);
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+
+            return View(schedule);
+        }
+
+        // POST: Schedules/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Schedule == null)
+            {
+                return Problem("Entity set 'SupermarketDbContext.Schedule'  is null.");
+            }
+            var schedule = await _context.Schedule.FindAsync(id);
+            if (schedule != null)
+            {
+                _context.Schedule.Remove(schedule);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ScheduleExists(int id)
+        {
+          return (_context.Schedule?.Any(e => e.ScheduleId == id)).GetValueOrDefault();
+        }
+    }
+}
