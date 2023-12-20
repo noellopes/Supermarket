a705b4a2c52e3f5b2aefe51b221fdf7c729ba80d
AntonioCerdeira <antonio.cerdeira@gmail.com>
2023-12-20 23:00:50 +0000

    Update SeedData

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -33,7 +33,11 @@ namespace Supermarket.Data
             if (db.Schedule.Any()) return;
 
             db.Schedule.AddRange(
-                    new Schedule { StartDate = DateTime.Now, EndDate = new DateTime(2028, 04, 30, 12, 30, 0), DailyStartTime = DateTime.Now, DailyFinishTime = DateTime.Now, Departments = db.Departments.FirstOrDefault(a => a.NameDepartments == "Talho")! }
+                    new Schedule { StartDate = DateTime.Now, EndDate = new DateTime(2028, 04, 30, 12, 30, 0), DailyStartTime = DateTime.Now, DailyFinishTime = DateTime.Now, Departments = db.Departments.FirstOrDefault(a => a.NameDepartments == "Talho")},
+                    new Schedule { StartDate = DateTime.Now, EndDate = new DateTime(2025, 04, 30, 12, 30, 0), DailyStartTime = DateTime.Now, DailyFinishTime = DateTime.Now, Departments = db.Departments.FirstOrDefault(a => a.NameDepartments == "Talho")},
+                    new Schedule { StartDate = DateTime.Now, EndDate = new DateTime(2028, 04, 30, 12, 30, 0), DailyStartTime = DateTime.Now, DailyFinishTime = DateTime.Now, Departments = db.Departments.FirstOrDefault(a => a.NameDepartments == "Talho") },
+                    new Schedule { StartDate = DateTime.Now, EndDate = new DateTime(2029, 04, 30, 12, 30, 0), DailyStartTime = DateTime.Now, DailyFinishTime = DateTime.Now, Departments = db.Departments.FirstOrDefault(a => a.NameDepartments == "Talho") },
+                    new Schedule { StartDate = DateTime.Now, EndDate = null, DailyStartTime = DateTime.Now, DailyFinishTime = DateTime.Now, Departments = db.Departments.FirstOrDefault(a => a.NameDepartments == "Talho") }
 
                 );
             db.SaveChanges();
