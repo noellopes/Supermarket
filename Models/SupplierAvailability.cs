50303921c9a7705a410ff3fbf3c1fc99fe8ae15d
Rodrigo Martins <rodasapm@hotmail.com>
2023-12-23 18:41:01 +0000

    Criação do model do SupplierAvailability

---
+++ Models/SupplierAvailability.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Supermarket.Models
+{
+    public class SupplierAvailability
+    {
+        [Key]
+        public int SupplierAvailabilityID { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public required string Name { get; set; }
+        public Product Product { get; set; }   
+
+
+        public double UnitPriceSupplier { get; set; }
+
+        public int ProductQuantity { get; set; }
+
+        public int DeliveryTime { get; set; }
+
+        public int SupplierId { get; set; }
+
+        public Supplier Supplier { get; set; }
+    }
+}
+
