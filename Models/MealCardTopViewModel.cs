b9a7e3d4bcbd10d478877b1bd4e96e9ecfb81627
arman <arman@ARMANDO-PC>
2023-12-31 15:35:46 +0000

    Create the TopMealCardsViewModel

---
+++ Models/MealCardTopViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Supermarket.Models
+{
+    public class MealCardTopViewModel
+    {
+
+        public List<Employee> Employees { get; set; }
+        public int EmployeeId { get; set; }
+
+        public string Employee_Name { get; set; }
+        public List<CardMovement> CardMovement { get; set; }
+        public List<MealCard> MealCards { get; set; }
+
+        public DateOnly Start_Filter { get; set; }
+        public DateOnly End_Filter { get; set; }
+
+        public int Total_Spent { get; set; }
+    }
+}
