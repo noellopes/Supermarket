743ccf9cd3803db1d1779985b02566402438726e
AntonioCerdeira <antonio.cerdeira@gmail.com>
2024-01-03 16:22:53 +0000

    Clean Data

--- Models/DepListViewModel.cs
+++ Models/DepListViewModel.cs
@@ -6,6 +6,5 @@ public class DepListViewModel
     public IEnumerable<Departments> Departments { get; set; }
     public PagingInfo Pagination { get; set; }
     public int SelectedPageSize { get; set; }
-    public List<Tickets> Tickets { get; set; }
-    public Dictionary<int, double> MediasPorDepartamento { get; set; }
+
 }
