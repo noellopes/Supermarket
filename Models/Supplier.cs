2ca119af413654b1d8cf143c3ed31354a1e78cc3
lucasleal <lucasoalzst@gmail.com>
2024-01-03 18:37:21 +0000

    Min and max character length for supplier name has been defined

--- Models/Supplier.cs
+++ Models/Supplier.cs
@@ -6,6 +6,9 @@ namespace Supermarket.Models
     {
         [Key]
         public int SupplierId { get; set; }
+
+        [Required]
+        [StringLength(40, MinimumLength = 3)]
         public string Name { get; set; } =  String.Empty;
 
         public ICollection<Issues>? Issue { get; set; }
