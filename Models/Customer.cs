833a8fc5154beadb359e0e8aa7bbaef417b20a3d
Melike Gökdemir <melikegkdemr@outlook.com>
2024-01-04 22:28:43 +0300

    Customer model and CRUD operations

---
+++ Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Supermarket.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string CustomerName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string CustomerEmail { get; set; }
+
+        [Required]
+        [Phone]
+        public string CustomerPhone { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string CustomerAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime CustomerBirth { get; set; }
+    }
+}
