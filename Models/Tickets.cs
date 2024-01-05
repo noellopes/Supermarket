0b3ea90a10a235f26fc31cf18c91da8f191daab0
Alexandre Rocha <1704920@sal.ipg.pt>
2024-01-05 23:14:12 +0000

    Inserção de tickets na SeedData

--- Models/Tickets.cs
+++ Models/Tickets.cs
@@ -7,23 +7,32 @@ namespace Supermarket.Models
         // Chave primária da entidade. O atributo [Key] é usado para marcar a chave primária.
         [Key]
         public int TicketId { get; set; }
-
         // Data de impressão do ticket.
         [Required]
         public DateTime DataEmissao { get; set; } = DateTime.Now;
-        //public DateTime DataEmissao { get; set; } = DateTime.Now ;
         // Data de impressão do ticket.
         public DateTime? DataAtendimento { get; set; } = null;
         // Número do ticket.
         [Required]
         public int NumeroDaSenha { get; set; }
-
         // Estado atual do ticket.
         public bool Estado { get; set; } = false;
 
         // Estado atual do ticket.
-        public bool Prioritario { get; set; } = false;
-
+        public bool Prioritario { get; set; }
+        //para calcular valores das datas 
+        public double TotalMinutesDifference
+        {
+            get
+            {
+                if (DataEmissao != default && DataAtendimento != default)
+                {
+                    TimeSpan difference = DataAtendimento - DataEmissao;
+                    return difference.TotalMinutes;
+                }
+                return 0; // se nada for feito retorna 
+            }
+        }
         // Chave estrangeira para o departamento. Relaciona-se a outra entidade chamada "Departments".
         public int IDDepartments { get; set; }
         public Departments? Departments { get; set; }
