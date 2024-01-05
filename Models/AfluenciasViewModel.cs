0d4b4e0347064142394e2e3e74ce1b8da757a409
andregod <andre_godinho1@hotmail.com>
2024-01-05 11:39:31 +0000

    Alterações afluencias e tickets

---
+++ Models/AfluenciasViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Supermarket.Models
+{
+    public class AfluenciasViewModel
+    {
+        public List<Tickets> Tickets { get; set; }
+
+        public Departments? Departments { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+
+        public DateTime? SearchDataIntervaloInicial { get; set; } = null;
+        public DateTime? SearchDataIntervaloFinal { get; set; } = null;
+
+        //public int SearchButtonDepartment { get; set; } = 0;
+    }
+}
