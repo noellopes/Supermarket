1720514a684715e8bc75bfc3d1c22761aa11074d
PINELA <pinelaivo10@gmail.com>
2024-01-05 17:41:06 +0000

    Adicionar a viewmodel para efeito de criação de um metodo de paginação na Warehouse

---
+++ Models/WarehouseViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Supermarket.Models
+{
+    public class WarehouseViewModel
+    {
+        public class ProductsWarehouseViewModel
+        {
+            public List<Warehouse> Warehouse{ get; set; }
+            public PagingInfoProduct PagingInfoProduct { get; set; }
+            public string SearchProductProduct { get; set; }
+            public string SearchProductBach { get; set; }
+
+        }
+    }
+}
