189ee75616cd6c2b2b698fb1dec0ed39a777c130
PINELA <pinelaivo10@gmail.com>
2024-01-05 19:05:14 +0000

    pagination and research carried out for warehouses

--- Models/WarehouseViewModel.cs
+++ Models/WarehouseViewModel.cs
@@ -2,13 +2,11 @@
 {
     public class WarehouseViewModel
     {
-        public class ProductsWarehouseViewModel
-        {
             public List<Warehouse> Warehouse{ get; set; }
             public PagingInfoProduct PagingInfoProduct { get; set; }
-            public string SearchProductProduct { get; set; }
-            public string SearchProductBach { get; set; }
+            public string SearchName { get; set; }
+            public string SearchAdress{ get; set; }
 
-        }
+        
     }
 }
