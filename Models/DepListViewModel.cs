af7d109190193e8d989cb99918dc048ee0147e78
Armando <arman@ARMANDO-PC>
2024-01-05 11:03:04 +0000

    Fixing some erros

---
+++ Models/DepListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Supermarket.Models;
+using System.Collections.Generic;
+
+public class DepListViewModel
+{
+    public IEnumerable<Departments> Departments { get; set; }
+    public PagingInfo Pagination { get; set; }
+    public int SelectedPageSize { get; set; }
+
+}
