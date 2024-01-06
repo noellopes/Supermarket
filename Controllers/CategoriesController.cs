83c966942d4f040660b4b385faac0127bec452fa
João Gonçalves <josantiago2033@gmail.com>
2024-01-06 14:16:24 +0000

    Added Search in Categories index

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -23,7 +23,7 @@ namespace Supermarket.Controllers
         // GET: Categories
         public async Task<IActionResult> Index(int page = 1, string name = "")
         {
-            var categories = from b in _context.Category select b;
+            var categories = _context.Category.AsQueryable();
 
             if (name != "")
             {
@@ -56,10 +56,45 @@ namespace Supermarket.Controllers
                 SearchName = name,
             };
 
-            //var totalCategories = await categories.CountAsync()
-            ViewBag.totalCategories = vm.PagingInfoProduct.TotalItems;
 
             return View(vm);
+            //var categories = from b in _context.Category select b;
+
+            //if (name != "")
+            //{
+            //    categories = categories.Where(x => x.Name.Contains(name));
+            //}
+
+            //PagingInfoProduct paging = new PagingInfoProduct
+            //{
+            //    CurrentPage = page,
+            //    TotalItems = await categories.CountAsync(),
+            //};
+
+            //if (paging.CurrentPage <= 1)
+            //{
+            //    paging.CurrentPage = 1;
+            //}
+            //else if (paging.CurrentPage > paging.TotalPages)
+            //{
+            //    paging.CurrentPage = paging.TotalPages;
+            //}
+
+            //var vm = new CategoriesViewModel
+            //{
+            //    Categories = await categories
+            //        .OrderBy(b => b.Name)
+            //        .Skip((paging.CurrentPage - 1) * paging.PageSize)
+            //        .Take(paging.PageSize)
+            //        .ToListAsync(),
+            //    PagingInfoProduct = paging,
+            //    SearchName = name,
+            //};
+
+            ////var totalCategories = await categories.CountAsync()
+            //ViewBag.totalCategories = vm.PagingInfoProduct.TotalItems;
+
+            //return View(vm);
             //return _context.Category != null ? 
             //              View(await _context.Category.OrderBy(c => c.Name).ToListAsync()) :
             //              Problem("Entity set 'SupermarketDbContext.Category'  is null.");
--- Models/CategoriesViewModel.cs
+++ Models/CategoriesViewModel.cs
@@ -5,5 +5,6 @@
         public List<Category> Categories { get; set; }
         public PagingInfoProduct PagingInfoProduct { get; set; }
         public string SearchName { get; set; } = string.Empty;
+
     }
 }
