4121cb5a067b6a02f7f28a98369423e24b0d0082
Armando <arman@ARMANDO-PC>
2024-01-09 21:16:36 +0000

    Sync (part 2)

--- Models/PagingInfo.cs
+++ Models/PagingInfo.cs
@@ -2,7 +2,9 @@
 {
     public class PagingInfo
     {
-        public const int NUMBER_PAGES_SHOW_BEFORE_AFTER = 3;
+        public const int DEFAULT_PAGE_SIZE = 10;
+
+        public const int NUMBER_PAGES_SHOW_BEFORE_AFTER = 5;
 
         public int TotalItems { get; set; }
 
