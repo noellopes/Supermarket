e1c772c9d7302a3419e4a9455a32b60b5b3ca8a3
Gabriel Couto <gabriel.couto14@hotmail.com>
2024-01-12 01:47:59 -0300

    Employee Schedule Completed

--- Models/EmployeeSchedulesViewModel.cs
+++ Models/EmployeeSchedulesViewModel.cs
@@ -3,8 +3,10 @@
     public class EmployeeSchedulesViewModel
     {
         public List<EmployeeSchedule> EmployeeSchedules { get; set; }
-
-        public int EmployeeId { get; set; }
-        public Employee? Employee { get; set; }
+        public int? EmployeeId { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int PageIndex { get; set; }
+        public int TotalPages { get; set; }
     }
 }
