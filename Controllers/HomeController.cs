ffe91199a28a0bb88eb37a1d9563c9806f72bb14
Rodrigoapm <rodasapm@hotmail.com>
2024-01-12 00:21:29 +0000

    Adicionado nav-item na nav-bar para redirecionar às tabelas do grupo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace Supermarket.Controllers {
             return View();
         }
 
+        public IActionResult Group1()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
